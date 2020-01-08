7b38e4645722a8b3321666d348940cb58bebf63e
DimaFursov <dimafursov@gmail.com>
2020-01-08 18:46:55 +0200

    MVCComment2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,13 @@ class ApplicationController < ActionController::Base
   # Браузер отправляет запрос, веб-сервер принимает этот запрос, и передаёт его в Rails-контроллер
   # Этот контролер ApplicationController routs.rb root куда 
   # Сейчас стартует class StaticPagesController < ApplicationController который наследует этот контролер 
-  # Вызов метода так как в routs.rb  путь  root 'static_pages#home'
+  # Вызов метода(действие) так как в routs.rb  путь  root 'static_pages#home'
   # def home (home.html.erb)
   # end
-  # models сейчас обходи и сразу идем в views к Rails.application.routes.draw do  application.html.erb  
+  # models сейчас обходит и сразу идет в views к Rails.application.routes.draw do  application.html.erb  
   # генерица страница home.html.erb
-  
+  #def hello
+  #  render text: "hello, world!"
+  #end
+  #
 end
