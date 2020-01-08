6d5378ad73f52e4a66239f022804fed5ee3f753b
DimaFursov <dimafursov@gmail.com>
2020-01-08 18:28:11 +0200

    MVCComment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  # MVC Controllers папка командует парадом над (GUI)views
+  # ПУп земли маршрутизатор! это контролер routs.rb root куда 
+  # Сейчас стартует class StaticPagesController < ApplicationController который наследует этот контролер 
+  # Вызов метода так как в routs.rb  путь  root 'static_pages#home'
+  # def home
+  # end
+  # генерица страница 
   
 end
