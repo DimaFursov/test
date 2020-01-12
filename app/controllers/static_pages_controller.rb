3f975ba84e29f18d86f5d32986d4790ce0588b58
DimaFursov <dimafursov@gmail.com>
2020-01-12 12:03:16 +0200

    stylesheet

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,10 +10,11 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
   def string_message(str = '')
   return "It's an empty string!" if str.noempty?
   return "Chui we're home"
-end
+  end
 # def string_message(str = '')
 #  return "It's an empty string!" if str.include?("foo")
 #  return "Chui we're home"
