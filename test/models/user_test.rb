3d41d1225a28d6282aba9c78692a3388b2b57261
DimaFursov <dimafursov@gmail.com>
2020-01-25 12:07:49 +0200

    testUser

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+#  Так как @user — это переменная экземпляра, то она автоматически доступна во всех тестах, и мы можем тестировать её валидность методом valid?
+  def setup
+    @user = User.new(name: "Example User", email: "user@example.com")
+  end
+# метод assert, который в данном случае будет успешным при возвращении true от @user.valid?, и провальным при возвращении false.
+  test "should be valid" do
+    assert @user.valid?
+  end
 end
