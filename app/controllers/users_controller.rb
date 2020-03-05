32d29868cce4dad16495b913fbe2f120087b68b5
DimaFursov <dimafursov@gmail.com>
2020-03-05 22:03:18 +0200

    migrate_password_digest

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -80,7 +80,7 @@ class UsersController < ApplicationController
        end
   end
 =end  
-  private
+  #private
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
@@ -99,7 +99,6 @@ class UsersController < ApplicationController
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
-
   end
 
   # DELETE /users/1
@@ -112,7 +111,6 @@ class UsersController < ApplicationController
     end
   end
 
-  private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
