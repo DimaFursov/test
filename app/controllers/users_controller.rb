3f272f33d79a0e7cf107e4cbceb5df2ae3407051
DimaFursov <dimafursov@gmail.com>
2020-03-05 21:04:50 +0200

    pizda1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -50,24 +50,41 @@ class UsersController < ApplicationController
     end
 =end 
   def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        redirect_to @user
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+=begin  
+  def create
       #user = User.new(params[:user])
     @user = User.new(user_params)
       respond_to do |format|
       if @user.save
+      flash[:success] = "Welcome to the Sample App!"        
+      redirect_to @user
       format.html { redirect_to @user, notice: 'User was successfully created.' }
       format.json { render :show, status: :created, location: @user }
       #redirect_to @user # Обработать успешное сохранение.
       else
+      render 'new'  
       format.html { render :new }
       format.json { render json: @user.errors, status: :unprocessable_entity }
-      render 'new'
-      end
+       end
   end
-  
+=end  
   private
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit()
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
   end  
 
@@ -101,7 +118,7 @@ class UsersController < ApplicationController
     def set_user
       @user = User.find(params[:id])
     end
-  end  
+  
 
 
 
