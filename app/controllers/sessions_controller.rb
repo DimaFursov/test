8b2ab18e1274c1a3434757f1ecb652d3a0d03f06
DimaFursov <dimafursov@gmail.com>
2020-03-07 17:44:48 +0200

    session new_html.erb2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,5 +4,18 @@ class SessionsController < ApplicationController
   end
   # post update
   # destroy DELETE к маршруту logout.
-  
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Осуществить вход пользователя и перенаправление на страницу профиля.
+    else
+      flash[:danger] = 'Invalid email/password combination' # Не совсем верно!          
+      # Выдать сообщение об ошибке. authenticate возвращает false при сбое аутентификации
+      render 'new'
+    end
+  end
+
+
+  def destroy
+  end
 end
