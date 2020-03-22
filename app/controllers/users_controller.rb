3d55f6651f5dc46a711e6b96e384c904f08f59b3
DimaFursov <dimafursov@gmail.com>
2020-03-22 16:41:26 +0200

    users2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -121,15 +121,16 @@ ivars:
   end  
 
   # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
+  # PATCH/PUT /users/1.json 
   def update
     respond_to do |format|
       if @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        render 'edit'
+        #format.html { render :edit }
+        #format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
   end
