7e1530c69596e9a99f327529955ebe46479d39a1
DimaFursov <dimafursov@gmail.com>
2020-03-29 23:58:08 +0300

    noAdminbrokenGravatar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
-  before_action :logged_in_user, only: [:index, :edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   # перенаправления пользователя
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   # GET /users
   # GET /users.json
 =begin
@@ -148,11 +149,14 @@ ivars:
 # DELETE /users/1
   # DELETE /users/1.json 
   def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+    #@user.destroy
+    #respond_to do |format|
+    #  format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+    #  format.json { head :no_content }
+    #end
   end
 
     # Use callbacks to share common setup or constraints between actions.
@@ -181,6 +185,10 @@ ivars:
       # unless @user == current_user
       redirect_to(root_url) unless current_user?(@user)
     end    
+    # Подтверждает администратора.
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
   #end  
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,19 @@
 User.create!(name:  "Example User",
              email: "example@railstutorial.org",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
+               
+User.create!(name:  "admin",
+             email: "admin@a.ca",
+             password:              "123123",
+             password_confirmation: "123123",
+             admin: true)
+User.create!(name:  "admin",
+             email: "admin2@a.ca",
+             password:              "123123",
+             password_confirmation: "123123",
+             admin: true)                 
 
 99.times do |n|
   name  = Faker::Name.name
@@ -18,7 +30,8 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password)
-end
+ 
+end              
 =begin
 bundle exec rake db:seed
 DEPRECATION WARNING: Sprockets method `register_engine` is deprecated.
