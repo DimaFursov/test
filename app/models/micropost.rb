1d5cc32246bc4049d8dd86d7b1707674bd6df7b7
DimaFursov <dimafursov@gmail.com>
2020-06-20 15:16:37 +0300

    fog

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -14,4 +14,13 @@ foo
   mount_uploader :picture, PictureUploader  
   validates :user_id, presence: true  
   validates :content, presence: true, length: { maximum: 140 }
+  validate  :picture_size
+  private
+
+    # Проводит валидацию размера загруженного изображения.
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "should be less than 5MB")
+      end
+    end  
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -1,7 +1,14 @@
 # encoding: utf-8
 
 class PictureUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process resize_to_limit: [400, 400]
 
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :file
+  end
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
@@ -15,7 +22,9 @@ class PictureUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
