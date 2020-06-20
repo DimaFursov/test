9b45ee9eeaab38a12180da5ee46fbbc6cae05e65
DimaFursov <dimafursov@gmail.com>
2020-06-20 14:19:47 +0300

    home with feed destroyz

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,6 +1,7 @@
 class MicropostsController < ApplicationController
   before_action :set_micropost, only: [:show, :edit, :update, :destroy]
   before_action :logged_in_user, only: [:create, :destroy]
+  before_action :correct_user,   only: :destroy
   # GET /microposts
   # GET /microposts.json
   
@@ -27,11 +28,22 @@ class MicropostsController < ApplicationController
   # POST /microposts
   # POST /microposts.json
   def create
+    @micropost = current_user.microposts.build(micropost_params)
+    if @micropost.save
+      flash[:success] = "Micropost created!"
+      redirect_to root_url
+    else
+      @feed_items = []
+      render 'static_pages/home'
+    end
+  end  
+=begin  
+  def create
     @micropost = Micropost.new(micropost_params)
 
     respond_to do |format|
       if @micropost.save
-        format.html { redirect_to @micropost, notice: 'Micropost was successfully created.' }
+        format.html { redirect_to @micropost, notice: 'Micropost was successfully created.' } #redirect_to @micropost,  #redirect_to root_url
         format.json { render :show, status: :created, location: @micropost }
       else
         format.html { render :new }
@@ -39,7 +51,7 @@ class MicropostsController < ApplicationController
       end
     end
   end
-
+=end
   # PATCH/PUT /microposts/1
   # PATCH/PUT /microposts/1.json
   def update
@@ -58,12 +70,18 @@ class MicropostsController < ApplicationController
   # DELETE /microposts/1.json
   def destroy
     @micropost.destroy
+    flash[:success] = "Micropost destroyz"
+    redirect_to request.referrer || root_url # перенаправление назад, на ту страницу, откуда был выдан запрос на удаление иначе на к
+  end  
+=begin  
+  def destroy
+    @micropost.destroy
     respond_to do |format|
       format.html { redirect_to microposts_url, notice: 'Micropost was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
-
+=end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_micropost
@@ -73,5 +91,9 @@ class MicropostsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def micropost_params
       params.require(:micropost).permit(:content, :user_id)
+    end
+     def correct_user
+      @micropost = current_user.microposts.find_by(id: params[:id])
+      redirect_to root_url if @micropost.nil?
     end
 end
